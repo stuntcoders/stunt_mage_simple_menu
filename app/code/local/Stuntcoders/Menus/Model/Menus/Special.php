4ed15f4b1232c2e141a3eda250e0bbf8b9def973
Mladen Ilic <mladen@stuntcoders.com>
2014-08-01 13:19:37 +0200

    Add wishlist url

--- app/code/local/Stuntcoders/Menus/Model/Menus/Special.php
+++ app/code/local/Stuntcoders/Menus/Model/Menus/Special.php
@@ -6,6 +6,7 @@ class Stuntcoders_Menus_Model_Menus_Special extends Mage_Core_Model_Abstract
     const MENU_ITEM_SPECIAL_LOGOUT      = 2;
     const MENU_ITEM_SPECIAL_CART        = 3;
     const MENU_ITEM_SPECIAL_CHECKOUT    = 4;
+    const MENU_ITEM_SPECIAL_WISHLIST    = 5;
 
     public static function getUrl($menuItem)
     {
@@ -14,6 +15,7 @@ class Stuntcoders_Menus_Model_Menus_Special extends Mage_Core_Model_Abstract
             case self::MENU_ITEM_SPECIAL_LOGOUT: return Mage::helper('customer')->getLogoutUrl(); break;
             case self::MENU_ITEM_SPECIAL_CART: return Mage::helper('checkout/url')->getCartUrl(); break;
             case self::MENU_ITEM_SPECIAL_CHECKOUT: return Mage::helper('checkout/url')->getCheckoutUrl(); break;
+            case self::MENU_ITEM_SPECIAL_WISHLIST: return Mage::getUrl('wishlist'); break;
             default: return ""; break;
         }
     }
