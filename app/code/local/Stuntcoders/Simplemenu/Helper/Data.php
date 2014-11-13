03b184613557a02cfef351fe1d7dd0f30987f45e
Mladen Ilic <mladen@stuntcoders.com>
2014-11-13 15:53:50 +0100

    Move simplemenu logic from helper to model

--- app/code/local/Stuntcoders/Simplemenu/Helper/Data.php
+++ app/code/local/Stuntcoders/Simplemenu/Helper/Data.php
@@ -2,47 +2,6 @@
 
 class Stuntcoders_Simplemenu_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public function getMenu($code)
-    {
-        $menuData = Mage::getModel('stuntcoders_simplemenu/simplemenu')
-            ->getCollection()
-            ->addCodeFilter($code)
-            ->getData();
-
-        if (!isset($menuData[0])) {
-            return array();
-        }
-
-        $menuData = $menuData[0];
-        $menuData["value"] = json_decode($menuData["value"], true);
-
-        $this->_formateMenuItems($menuData["value"]);
-
-        return $menuData;
-    }
-
-    private function _outputMenu($menu)
-    {
-        $out = "";
-
-        foreach ($menu as $menuItem) {
-            $out .= "<li><a href='{$menuItem['url']}'>{$menuItem['label']}</a>";
-
-            if (!empty($menuItem['children'])) {
-                $out .= "<ul>" . $this->_outputMenu($menuItem['children']) . "</ul>";
-            }
-            $out .= "</li>";
-        }
-
-        return $out;
-    }
-
-    public function getMenuOutput($code)
-    {
-        $mainMenu = Mage::helper('stuntcoders_simplemenu')->getMenu('main_menu');
-        return "<ul id='$code'>" . $this->_outputMenu($mainMenu['value']) . "</ul>";
-    }
-
     public function getCategoriesArray()
     {
         return Mage::getModel('catalog/category')
@@ -51,16 +10,6 @@ class Stuntcoders_Simplemenu_Helper_Data extends Mage_Core_Helper_Abstract
             ->addAttributeToSort('path', 'asc')
             ->load()
             ->toArray();
-    }
-
-    private function _formateMenuItems(&$menuItems)
-    {
-        foreach ($menuItems as &$menuItem) {
-            Stuntcoders_Simplemenu_Model_Simplemenu::formatMenuItem($menuItem);
-            if (isset($menuItem["children"])) {
-                $this->_formateMenuItems($menuItem["children"]);
-            }
-        }
     }
 
     public function getMenuItemDataUrl($menuItem)
@@ -75,7 +24,8 @@ class Stuntcoders_Simplemenu_Helper_Data extends Mage_Core_Helper_Abstract
     public function getMenuItemDataId($menuItem)
     {
         if ((int)$menuItem['type'] !== Stuntcoders_Simplemenu_Model_Simplemenu::MENU_ITEM_TYPE_CATEGORY &&
-            (int)$menuItem['type'] !== Stuntcoders_Simplemenu_Model_Simplemenu::MENU_ITEM_TYPE_CMS_PAGE) {
+            (int)$menuItem['type'] !== Stuntcoders_Simplemenu_Model_Simplemenu::MENU_ITEM_TYPE_CMS_PAGE &&
+            (int)$menuItem['type'] !== Stuntcoders_Simplemenu_Model_Simplemenu::MENU_ITEM_TYPE_SPECIAL) {
             return "";
         }
 
--- app/code/local/Stuntcoders/Simplemenu/Model/Simplemenu.php
+++ app/code/local/Stuntcoders/Simplemenu/Model/Simplemenu.php
@@ -12,36 +12,96 @@ class Stuntcoders_Simplemenu_Model_Simplemenu extends Mage_Core_Model_Abstract
         $this->_init('stuntcoders_simplemenu/simplemenu');
     }
 
-    public static function formatMenuItem(&$menuItem)
+    public function getMenu($code)
+    {
+        $menuId = Mage::getModel('stuntcoders_simplemenu/simplemenu')
+            ->getCollection()
+            ->addCodeFilter($code)
+            ->getFirstItem()->getId();
+
+        // Menu with such code does not exist
+        if (!$menuId) {
+            return array();
+        }
+        $this->load($menuId);
+
+        $menu = $this->_getMenuData();
+
+        $this->_formatMenu($menu);
+
+        return $menu;
+    }
+
+    public function getMenuOutput($code)
+    {
+        $menu = $this->getMenu($code);
+        return "<ul id='$code'>" . $this->_outputMenu($menu['value']) . "</ul>";
+    }
+
+    protected function _formatMenu(&$menu)
+    {
+        foreach ($menu as &$menuItem) {
+            $menuItem = $this->_formatMenuItem($menuItem);
+            if (isset($menuItem["children"])) {
+                $this->_formatMenu($menuItem["children"]);
+            }
+        }
+    }
+
+    public function _formatMenuItem($menuItem)
     {
         if (!is_array($menuItem) || empty($menuItem)) {
-            return;
+            return array();
         }
 
         if (!isset($menuItem["type"])) {
-            return;
+            return array();
         }
 
         switch ((int)$menuItem["type"]) {
             case self::MENU_ITEM_TYPE_LINK:
+                $model = Mage::getModel('stuntcoders_simplemenu/simplemenu_link');
                 break;
             case self::MENU_ITEM_TYPE_CATEGORY:
-                Stuntcoders_Simplemenu_Model_Simplemenu_Category::formatMenuItem($menuItem);
+                $model = Mage::getModel('stuntcoders_simplemenu/simplemenu_category');
                 break;
             case self::MENU_ITEM_TYPE_CMS_PAGE:
-                Stuntcoders_Simplemenu_Model_Simplemenu_Cms::formatMenuItem($menuItem);
+                $model = Mage::getModel('stuntcoders_simplemenu/simplemenu_cms');
                 break;
             case self::MENU_ITEM_TYPE_SPECIAL:
-                Stuntcoders_Simplemenu_Model_Simplemenu_Special::formatMenuItem($menuItem);
+                $model = Mage::getModel('stuntcoders_simplemenu/simplemenu_special');
                 break;
             default:
-                unset($menuItem);
+                return array();
                 break;
         }
 
-        unset($menuItem['typename']);
-        unset($menuItem['subcategories']);
-        unset($menuItem['id']);
-        unset($menuItem['type']);
+//        unset($menuItem['typename']);
+//        unset($menuItem['subcategories']);
+//        unset($menuItem['id']);
+//        unset($menuItem['type']);
+
+        return $model->formatMenuItem($menuItem);
+    }
+
+    protected function _getMenuData()
+    {
+        return json_decode($this->getValue(), true);
+    }
+
+    protected function _outputMenu($menu)
+    {
+        $out = "";
+
+        foreach ($menu as $menuItem) {
+            $out .= "<li><a href='{$menuItem['url']}'>{$menuItem['label']}</a>";
+
+            if (!empty($menuItem['children'])) {
+                $out .= "<ul>" . $this->_outputMenu($menuItem['children']) . "</ul>";
+            }
+            $out .= "</li>";
+        }
+
+        return $out;
     }
 }
